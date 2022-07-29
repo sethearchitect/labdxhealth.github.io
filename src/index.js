7d112caa2738feb498a66b51cf1af02456c56f75
sethearchitect <se.thearchitect@gmail.com>
2022-07-29 08:29:17 +0100

    added script for dropdown functionality

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-const dropdownTabs = document.querySelectorAll('[data-dropdown]');
+const dropdownTabs = document.querySelectorAll('dropdown-tab');
+
+//Product panel title tab
 const panelNames = document.querySelectorAll('.tab-title');
+//Product panel dropdown content
 const dropdownContent = document.getElementsByClassName('panel-drop-content');
+//product panels dropdown button
+const dropdownbtn = document.querySelectorAll('#dropdown-btn');
+//FAQ question title tabs
+const questions = document.querySelectorAll('.question');
 
 //Dynamically set height of dropdown name label
+function setDynamicHeight(elem) {
+    let height = elem.children[0].offsetHeight;
+    elem.style.height = `${height}px`;
+}
 window.addEventListener('resize', () => {
     dropdownTabs.forEach(dropdownTab => {
-        let height = dropdownTab.children[0].offsetHeight;
-        dropdownTab.style.height = `${height}px`
+        setDynamicHeight(dropdownTab);
+    });
+    questions.forEach(question => {
+        setDynamicHeight(question);
     });
 })
 
 
 
-//Click dropdown
-const workingClass = dropdownContent.classList;
-
-// console.log(tabToggle);
-//Add event listeners to tab title divs
-panelNames.forEach(e => {
-    e.addEventListener('click', dropDown)
+//Add event listeners to product panel dropdown titles
+panelNames.forEach(panel => {
+    panel.addEventListener('click', dropDown)
 })
 
 //Event Handler function
@@ -26,24 +35,22 @@ function dropDown(event) {
 
     if (!panelNames && event.target.closest('.dropdown-tab') != null) return;
     let currentDropdown;
-    // let dropContent;
+    let currentDropdownBtn;
     if (panelNames) {
+
         currentDropdown = event.target.closest('.dropdown-tab');
+
+        //dropdown btn transition on expand
+        currentDropdownBtn = event.target.closest('.tab-title').children[1];
+        currentDropdownBtn.classList.toggle('rotate');
+
         if (currentDropdown.style.height != '100%') {
             currentDropdown.style.height = '100%';
         } else {
             currentDropdown.style.height = `${currentDropdown.children[0].offsetHeight}px`
         }
+
+
     }
 
 }
-// tabTest.addEventListener('click', e => {
-//     console.log(dropdownContent);
-
-//     if (dropdownContent.style.display == 'none') {
-//         dropdownContent.style.display = 'block';
-
-//     } else {
-//         dropdownContent.style.display = 'none';
-//     }
-// })
