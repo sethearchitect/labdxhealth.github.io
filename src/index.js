362173bdeead19a4a5774909d2a692e9bf53f440
sethearchitect <se.thearchitect@gmail.com>
2022-08-01 14:12:06 +0100

    resolved dropdown bug for product panels

--- src/index.js
+++ src/index.js
@@ -10,47 +10,65 @@ const dropdownbtn = document.querySelectorAll('#dropdown-btn');
 const questions = document.querySelectorAll('.question');
 
 //Dynamically set height of dropdown name label
-function setDynamicHeight(elem) {
-    let height = elem.children[0].offsetHeight;
-    elem.style.height = `${height}px`;
-}
-window.addEventListener('resize', () => {
-    dropdownTabs.forEach(dropdownTab => {
-        setDynamicHeight(dropdownTab);
-    });
-    questions.forEach(question => {
-        setDynamicHeight(question);
-    });
-})
+// function setDynamicHeight(elem) {
+//     let height = elem.children[0].offsetHeight;
+//     elem.style.height = `${height}px`;
+// }
+// window.addEventListener('resize', () => {
+//     dropdownTabs.forEach(dropdownTab => {
+//         setDynamicHeight(dropdownTab);
+//     });
+//     questions.forEach(question => {
+//         setDynamicHeight(question);
+//     });
+// })
 
 
 
 //Add event listeners to product panel dropdown titles
 panelNames.forEach(panel => {
-    panel.addEventListener('click', dropDown)
+    panel.addEventListener('click', dropDown);
+    console.log(panel);
 })
 
 //Event Handler function
-function dropDown(event) {
+// function dropDown(event) {
 
-    if (!panelNames && event.target.closest('.dropdown-tab') != null) return;
-    let currentDropdown;
-    let currentDropdownBtn;
-    if (panelNames) {
+//     console.log(event);
 
-        currentDropdown = event.target.closest('.dropdown-tab');
+//     // if (!panelNames && event.target.closest('.dropdown-tab') != null) return;
+//     let currentDropdown;
+//     let currentDropdownBtn;
+//     if (panelNames) {
 
-        //dropdown btn transition on expand
-        currentDropdownBtn = event.target.closest('.tab-title').children[1];
-        currentDropdownBtn.classList.toggle('rotate');
+//         currentDropdown = event.target.closest('.dropdown-tab');
 
-        if (currentDropdown.style.height != '100%') {
-            currentDropdown.style.height = '100%';
-        } else {
-            currentDropdown.style.height = `${currentDropdown.children[0].offsetHeight}px`
-        }
+//         //dropdown btn transition on expand
+//         currentDropdownBtn = event.target.closest('.tab-title').children[1];
+//         currentDropdownBtn.classList.toggle('rotate');
+
+//         if (currentDropdown.style.height != '100%') {
+//             currentDropdown.style.height = '100%';
+//         } else {
+//             currentDropdown.style.height = `${currentDropdown.children[0].offsetHeight}px`
+//         }
 
 
+//     }
+
+// }
+
+function dropDown() {
+    let parent = this.closest('.dropdown-tab');
+    let btn = this.children[1];
+    btn.classList.toggle('rotate');
+    let content = this.nextElementSibling;
+
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        parent.style.rowGap = null;
+    } else {
+        parent.style.rowGap = '16px';
+        content.style.maxHeight = content.scrollHeight + "px";
     }
-
 }
